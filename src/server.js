41c5de5feae6e98a1e3e440f20eba6df4f847531
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2022-10-28 12:44:17 +0600

    update port

--- src/server.js
+++ src/server.js
@@ -1,31 +1,30 @@
-const { cpus } = require("os")
-const cluster = require("cluster")
+const { cpus } = require("os");
+const cluster = require("cluster");
 
-const app = require("./app")
-const dbConnection = require("./config/db.config")
+const app = require("./app");
+const dbConnection = require("./config/db.config");
 
-const numCPUs = cpus().length
-const PORT = process.env.APP_PORT || 5000
+const numCPUs = cpus().length;
+const PORT = process.env.PORT || 5000;
 
 /* Clustering the apllication */
 if (cluster.isMaster) {
-    console.log(`Primary ${process.pid} is running`)
+  console.log(`Primary ${process.pid} is running`);
 
-    /* Fork workers. */
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-    }
+  /* Fork workers. */
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    cluster.on("exit", (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`)
-    })
-}
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
 /* Run application via port & Establish database connection */
-else {
-    app.listen(PORT, () => {
-        dbConnection()
+  app.listen(PORT, () => {
+    dbConnection();
 
-        console.log(`App running on http://localhost:${PORT}`)
-        console.log(`Worker ${process.pid} started`);
-    })
+    console.log(`App running on http://localhost:${PORT}`);
+    console.log(`Worker ${process.pid} started`);
+  });
 }
