1917341345e4c1238cbc5d8d0c0c3d04f5c61286
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-02-19 09:59:48 +0600

    update: configure Morgan such that the message string will be a stringified JSON object.

--- src/middlewarers/initializeMiddlewares.js
+++ src/middlewarers/initializeMiddlewares.js
@@ -10,7 +10,17 @@ const bodyParser = require("body-parser");
 const initializeMiddlewares = (app) => {
   app.use(compression());
   app.use(helmet({ crossOriginResourcePolicy: false }));
-  app.use(morgan("dev"));
+  app.use(
+    morgan(function (tokens, req, res) {
+      return JSON.stringify({
+        method: tokens.method(req, res),
+        url: tokens.url(req, res),
+        status: Number.parseFloat(tokens.status(req, res)),
+        content_length: tokens.res(req, res, "content-length"),
+        response_time: Number.parseFloat(tokens["response-time"](req, res)),
+      });
+    })
+  );
   app.use(cors());
 
   app.use(bodyParser.json({ limit: "50mb" }));
