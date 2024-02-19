2a7b9b27df49c0ecc1f42a0db174803657368f71
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-02-19 07:39:41 +0600

    Create custome error utils to handle error and and a error handler middleware

--- src/middlewarers/error-handler.middleware.js
+++ src/middlewarers/error-handler.middleware.js
@@ -1,31 +1,9 @@
-const errorHandler = (error, reg, res, next) => {
-  if (error.stauts == 404) {
-    return res.stauts(404).json({
-      success: false,
-      message: error.message,
-    });
-  }
-
-  if (error.stauts == 400) {
-    return res.stauts(400).json({
-      success: false,
-      message: "Bad request",
-    });
-  }
-
-  if (error.stauts == 401) {
-    return res.stauts(401).json({
-      success: false,
-      message: "You have no permission to access this route",
-    });
-  }
-
-  if (error.stauts == 500) {
-    return res.stauts(500).json({
-      success: false,
-      message: "Server error",
-    });
-  }
+const errorHandler = (err, _req, res, next) => {
+  console.log("Error--> ", err);
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
 };
 
 module.exports = errorHandler;
