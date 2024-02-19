0eaa98c89c59fe3594b50a2551f071e9b156c416
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-02-19 07:48:55 +0600

    Refactor: seperate env config

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const configureSwagger = require("./config/swagger.config");
+const configEnv = require("./config/dotenv.config");
 const applyMiddleware = require("./middlewarers/applyMiddleware");
 const { bindUserWithReq } = require("./middlewarers/authMiddleware");
 
 const errorHandler = require("./middlewarers/error-handler.middleware");
 const setupRoutes = require("./routes");
 
-dotenv.config();
-
 const app = express();
+
+/**Dot env config */
+configEnv();
 /**Swagger config with app */
 configureSwagger(app);
 /**Middleware config  with app*/
@@ -22,13 +24,6 @@ app.use(bindUserWithReq());
 
 app.get("/", async (req, res) => {
   res.send("Wow!😯 are you here🙃🙃 application running!!! 😜😜😜");
-});
-
-/* 404 page handelling */
-app.use((req, res, next) => {
-  let error = new Error("404 page not found.");
-  error.status = 404;
-  next(error);
 });
 
 /* Error handler middleware */
