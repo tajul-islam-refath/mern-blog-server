a82d6131d577b2493d0ef135e8c7774a5ea1d92e
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-02-29 23:33:22 +0600

    fixed: Param id validation. des: When use custom validation in express-validator if condation valid always return true other wise through an error

--- src/validations/paramValidation.js
+++ src/validations/paramValidation.js
@@ -1,15 +1,15 @@
-const { body, param } = require("express-validator");
+const { param } = require("express-validator");
 const mongoose = require("mongoose");
 
 const paramIdValidation = [
   param("id")
-    .bail()
     .notEmpty()
     .withMessage("Param ID is required")
     .custom((id) => {
       if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new Error("The provided ID is not a valid MongoDB ObjectId");
       }
+      return true;
     }),
 ];
 
